7c8ab626cc4d395ffff0c1e5118d2a1fddbc1400
nano.seabra <nano.seabra@f16163c0-9e0f-11de-a8ff-c955b97a4a10>
2009-09-11 00:12:09 +0000

    - Criação do Cubo e da Esfera estão agora em outro arquivo - Fiz alguns testes com selection e picking, sem muito sucesso... os testes estão comentados - Transformei os pixels em coordenadas do opengl (usando gluUnProject) - A translação funciona, mas não fica exatamente onde o mouse está; talvez tenha que ter algum ajuste que não tenho conhecimento

--- src/glwidget.py
+++ src/glwidget.py
@@ -2,66 +2,134 @@ from PyQt4.QtOpenGL import *
 from PyQt4.QtGui import *
 from OpenGL.GL import *
 from OpenGL.GLU import *
-from OpenGL.raw.GLUT import glutWireCube, glutWireSphere
+from OpenGL.raw.GLUT import *
+
+from objects import *
 
 class GlWidget(QGLWidget):
-	"""Our implementation of QGLWidget widget. :) - Joaquim, el gato."""
-	
-	def __init__ (self, parent=None):
-		"""Constructor."""
-		super(GlWidget, self).__init__(parent)
-		self.esfera = False
-		self.cubinho = False
-		
-	def paintGL(self):
-		"""A Simple drawing callback for drawing one triangle"""
-		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
-		glRotate(2, 0.5, 0.9, 0.5)
-		if (self.esfera):
-			glColor3f(1.0, 0, 0)
-			glutWireSphere(0.5, 40, 20) #RENAN, volte para casa, Renan...
-		if (self.cubinho):
-			glColor3f(1.0, 0, 1.0)
-			glutWireCube(0.5)
+    """Our implementation of QGLWidget widget. :) - Joaquim, el gato."""
+    
+    def __init__ (self, parent=None):
+        """Constructor"""
+        
+        super(GlWidget, self).__init__(parent)
+        self.sphere = False
+        self.cube = False
+        
+        self.mousePosX = 0.0
+        self.mousePoxY = 0.0
+        self.mousePosZ = 0.0
+        
+        self.coordPosition = []
+        
+    def paintGL(self):
+        """A Simple drawing callback for drawing one triangle"""
+        
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+        #glRotate(2, 0.5, 0.9, 0.5)
+        self.draw()
+        #self.drawing()
+        
+#    def selection(self):
+#        glLoadName(1)
+#        Cube(0.5)
+#        
+#        glLoadName(2)
+#        Sphere(0.5)
+#        
+#        hits = glRenderMode(GL_RENDER)
+#        print "Hits = ", hits
+#    
+#    def drawing(self):
+#        Cube(0.5)
+#        Sphere(0.5)
+#        
+#    def picking(self):
+#        viewport = glGetIntegerv(GL_VIEWPORT)
+#        print "Viewport = ", viewport
+#        print "Viewport[3] = ", viewport[3]
+#        
+#        glSelectBuffer(512)
+#        glRenderMode(GL_SELECT)
+#        
+#        glInitNames()
+#        glPushName(0)
+#        
+#        glMatrixMode(GL_PROJECTION)
+#        glPushMatrix()
+#        glLoadIdentity()
+#        
+#        gluPickMatrix(QCursor.pos().x(), viewport[3] - QCursor.pos().y(),
+#                      5.0, 5.0, viewport)
+#        
+#        gluPerspective(45, 1.3, 10, 40)
+#        
+#        self.selection()
+#        glMatrixMode(GL_PROJECTION)
+#        glPopMatrix()
+#        glFlush()
 
-	def resizeGL (self, width, height):
-		"""A simple resize callback."""
-		side = min(width,height)
-		glViewport((width - side) / 2, (height - side) / 2, side, side)
-#		glMatrixMode(GL_PROJECTION)
-#		glLoadIdentity()
-#		gluPerspective(60, width/height, 1.0, 100.0)
-		glMatrixMode(GL_MODELVIEW)
-#		glLoadIdentity()
-#		glTranslatef(-0.5, -0.5, 0)
-#		glRotate(45, 0, 0, -1.0)
+    def resizeGL (self, width, height):
+        """A simple resize callback."""
+        
+        side = min(width,height)
+        glViewport((width - side) / 2, (height - side) / 2, side, side)
+        glMatrixMode(GL_MODELVIEW)
 
-	def mouseMoveEvent(self, ev):
-		print "Moh-vel"
-		
-	def mousePressEvent(self, ev):
-		print "Clico! :D"
-		
-	def mouseReleaseEvent(self, ev):
-		print "Solto... :(" 			
+#    def mouseMoveEvent(self, ev):
+#        print "Moh-vel"
+#        
+#    def mousePressEvent(self, ev):
+#        print "Clico! ;D"
+#        self.picking()
+#        print "Type = ", ev.type()
+#        
+#    def mouseReleaseEvent(self, ev):
+#        print "Solto... :("             
+#
+    def enterEvent(self, ev):
+        self.setFocus()
+        self.grabKeyboard()
+        
+    def leaveEvent(self, ev):
+        self.parent().setFocus()
+        self.releaseKeyboard()
 
-	def enterEvent(self, ev):
-		self.setFocus()
-		self.grabKeyboard()
-		
-	def leaveEvent(self, ev):
-		self.parent().setFocus()
-		self.releaseKeyboard()
+    def keyPressEvent(self, ev):
+        key = str(ev.text()).upper()
+        if (key == "C"):
+            self.cube = True
+            self.update()
+        elif (key == "E"):
+            self.sphere = True
+            self.update()
+        else:
+            pass
+            
+    def draw(self):
+        self.mousePosX = self.mapFromGlobal(QCursor.pos()).x()
+        self.mousePoxY = self.mapFromGlobal(QCursor.pos()).y()
+        
+#        self.mousePosZ = glReadPixels(self.mousePosX, self.mousePoxY, 1, 1, 
+#                                      GL_DEPTH_COMPONENT, GL_FLOAT)
+        self.mousePosZ = 1
 
-	def keyPressEvent(self, ev):
-		#ctrl, shift = self._GetCtrlShift(ev)
-		key = str(ev.text()).upper()
-		if (key == "C"):
-			self.cubinho = not self.cubinho
-			self.update()
-		elif (key == "E"):
-			self.esfera = not self.esfera
-			self.update()
-		elif (key == "R"):
-			self.update()
-			print QCursor.pos()
+        self.coordPosition = gluUnProject(self.mousePosX, self.mousePoxY, self.mousePosZ,
+										  glGetDoublev(GL_MODELVIEW_MATRIX),
+										  glGetDoublev(GL_PROJECTION_MATRIX),
+										  glGetIntegerv(GL_VIEWPORT))
+        
+        if (self.sphere):
+            glColor3f(1.0, 0, 0)
+            Sphere(0.5)
+            glMatrixMode(GL_MODELVIEW)
+            glTranslatef(self.coordPosition[0], self.coordPosition[1],
+						 self.coordPosition[2])
+            self.sphere = False
+        if (self.cube):
+            glColor3f(1.0, 0, 1.0)
+            Cube(0.5)
+            glMatrixMode(GL_MODELVIEW)
+            glTranslatef(self.coordPosition[0], self.coordPosition[1],
+						 self.coordPosition[2])
+            self.cube = False
