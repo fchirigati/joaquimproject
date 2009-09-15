77bd2ae15bc8e0d4111ac69a795a001406e5a23d
nano.seabra <nano.seabra@f16163c0-9e0f-11de-a8ff-c955b97a4a10>
2009-09-15 03:34:10 +0000

    - Código do Renan (zoom + lightning) - Picking (ainda não está funcionando 100%)

--- src/glwidget.py
+++ src/glwidget.py
@@ -1,8 +1,9 @@
 from PyQt4.QtOpenGL import *
 from PyQt4.QtGui import *
+from PyQt4 import QtCore
 from OpenGL.GL import *
 from OpenGL.GLU import *
-from OpenGL.raw.GLUT import *
+from OpenGL.GLUT import *
 
 from objects import *
 
@@ -18,57 +19,20 @@ class GlWidget(QGLWidget):
 		
 		self.sceneObjects = []
 		self.selectedObject = None
-		#self.boundingSphere = Sphere(radius, wire)
 		self.mousePosX = 0.0
 		self.mousePosY = 0.0
 		self.mousePosZ = 0.0
+		self.move = 0.0
+		self.z = 0.0
+		self.zoom = 2.0
+		self.applyZoom = 0
+		
 		
 	def paintGL(self):
 		"""A Simple drawing callback for drawing one triangle"""
 		
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
-		#glRotate(2, 0.5, 0.9, 0.5)
 		self.render()
-		#self.drawing()
-		
-#	def selection(self):
-#		glLoadName(1)
-#		Cube(0.5)
-#		
-#		glLoadName(2)
-#		Sphere(0.5)
-#		
-#		hits = glRenderMode(GL_RENDER)
-#		print "Hits = ", hits
-#	
-#	def drawing(self):
-#		Cube(0.5)
-#		Sphere(0.5)
-#		
-#	def picking(self):
-#		viewport = glGetIntegerv(GL_VIEWPORT)
-#		print "Viewport = ", viewport
-#		print "Viewport[3] = ", viewport[3]
-#		
-#		glSelectBuffer(512)
-#		glRenderMode(GL_SELECT)
-#		
-#		glInitNames()
-#		glPushName(0)
-#		
-#		glMatrixMode(GL_PROJECTION)
-#		glPushMatrix()
-#		glLoadIdentity()
-#		
-#		gluPickMatrix(QCursor.pos().x(), viewport[3] - QCursor.pos().y(),
-#					  5.0, 5.0, viewport)
-#		
-#		gluPerspective(45, 1.3, 10, 40)
-#		
-#		self.selection()
-#		glMatrixMode(GL_PROJECTION)
-#		glPopMatrix()
-#		glFlush()
 
 	def resizeGL (self, width, height):
 		"""A simple resize callback."""
@@ -76,60 +40,77 @@ class GlWidget(QGLWidget):
 		side = min(width,height)
 		glViewport((width - side) / 2, (height - side) / 2, side, side)
 		glMatrixMode(GL_MODELVIEW)
+		#glLoadIdentity()
+		#gluLookAt (0.0, 0.0, self.zoom, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0)
+		self.updateGL()
 
 	def mouseMoveEvent(self, ev):
 		self.updateMousePosition()
+		if self.applyZoom:
+			if self.z > self.zoom:
+				self.zoom-=abs(self.zoom - self.z)/500
+				glLoadIdentity()
+				gluLookAt (self.move, 0.0, self.zoom, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0)
+				self.updateGL()
+			else:
+				self.zoom+=abs(self.zoom - self.z)/500
+				glLoadIdentity()
+				gluLookAt (self.move, 0.0, self.zoom, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0)
+				self.updateGL() 
 		self.update()
 		
 	def mousePressEvent(self, ev):
-#		print "Clico! ;D"
-#		self.picking()
-		print "Type:", ev.type()
+		print "Clico! ;D"
+		self.pickingObjects(self.mousePosX, self.mousePosY)
+		if ev.button() == QtCore.Qt.RightButton:
+			self.applyZoom = 1
+			self.z = self.mousePosY
 #		
 	def mouseReleaseEvent(self, ev):
-		print "Solto... :("			 
+		print "Solto... :("
+		if ev.button() == QtCore.Qt.RightButton:
+			self.applyZoom = 0		 
 
-<<<<<<< .mine    def keyPressEvent(self, ev):
-        key = str(ev.text()).upper()
-        if (key == "C"):
-            self.cube = True
-            self.update()
-        elif (key == "E"):
-            self.sphere = True
-            self.update()
-        else:
-            pass        
-    
-    def draw(self):
-        self.mousePosX = self.mapFromGlobal(QCursor.pos()).x()
-        self.mousePosY = self.mapFromGlobal(QCursor.pos()).y()
-        
-#        self.mousePosZ = glReadPixels(self.mousePosX, self.mousePoxY, 1, 1, 
-#                                      GL_DEPTH_COMPONENT, GL_FLOAT)
-        self.mousePosZ = 1
-        
-        #Para que o movimento do mouse seja reconhecido sem se pressionar nenhum botao do mouse
-        self.setMouseTracking(True)
-=======	def enterEvent(self, ev):
+	def enterEvent(self, ev):
 		self.setFocus()
 		self.grabKeyboard()
 		
 	def leaveEvent(self, ev):
 		self.parent().setFocus()
 		self.releaseKeyboard()
->>>>>>> .theirs
+
 	def keyPressEvent(self, ev):
 		key = str(ev.text()).upper()
 		if (key == "C"):
-			newCube = Cube(0.5)
+			newCube = Cube(0.5, False)
 			newCube.setCentralPosition(self.getMouseScenePosition())
 			self.sceneObjects.append(newCube)
 			self.update()
 		elif (key == "E"):
-			newSphere = Sphere(0.5)
+			newSphere = Sphere(0.5, False)
 			newSphere.setCentralPosition(self.getMouseScenePosition())
 			self.sceneObjects.append(newSphere)
 			self.update()
+		elif (key == "W"):
+			self.zoom-=0.2
+			glLoadIdentity()
+			gluLookAt (self.move, 0.0, self.zoom, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0)
+			self.updateGL()
+		elif (key == "S"):
+			self.zoom+=0.2
+			glLoadIdentity()
+			gluLookAt (self.move, 0.0, self.zoom, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0)
+			self.updateGL()
+		elif (key == "A"):
+			self.move-=0.3
+			glLoadIdentity()
+			gluLookAt (self.move, 0.0, self.zoom, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0)
+			self.updateGL()
+		elif (key == "D"):
+			self.move+=0.3
+			glLoadIdentity()
+			gluLookAt (self.move, 0.0, self.zoom, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0)
+			self.updateGL()
 		else:
 			pass
 		
@@ -138,9 +119,9 @@ class GlWidget(QGLWidget):
 		self.mousePosY = self.mapFromGlobal(QCursor.pos()).y()
 			
 	def getMouseScenePosition(self):
-		self.mousePosZ = glReadPixels(self.mousePosX, self.mousePosY, 1, 1, 
-									  GL_DEPTH_COMPONENT, GL_FLOAT)
-		#self.mousePosZ = 0.1
+#		self.mousePosZ = glReadPixels(self.mousePosX, self.mousePosY, 1, 1,
+#									  GL_DEPTH_COMPONENT, GL_FLOAT)
+		self.mousePosZ = 0.1
 
 		adjustedY = glGetIntegerv(GL_VIEWPORT)[3] - self.mousePosY - 1
 		
@@ -152,11 +133,83 @@ class GlWidget(QGLWidget):
 		return self.coordPosition
 		
 	def render(self):
+		
 		glMatrixMode(GL_MODELVIEW)
-		glLoadIdentity()
-		glColor3f(1.0, 0, 0)
+		
+		glEnable(GL_LIGHTING)
+		glEnable(GL_LIGHT0)
+		glEnable(GL_COLOR_MATERIAL)
+		
+		light0_pos = 40.0, 0.0, 0.0, 100.0
+		diffuse0 = 1.0, 1.0, 1.0, 1.0
+		specular0 = 0, 0, 0, 1.0
+		ambient0 = 0, 0, 0, 1.0
+
+		glMatrixMode(GL_MODELVIEW)
+		glLightfv(GL_LIGHT0, GL_POSITION, light0_pos)
+		glLightfv(GL_LIGHT0, GL_DIFFUSE, diffuse0)
+		glLightfv(GL_LIGHT0, GL_SPECULAR, specular0)
+		glLightfv(GL_LIGHT0, GL_AMBIENT, ambient0)
+		
 		for obj in self.sceneObjects:
 			glPushMatrix()
 			obj.render()
 			glPopMatrix()
+
+		glLoadIdentity()
+		gluLookAt (self.move, 0.0, self.zoom, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0)
+			
+	def pickingObjects(self, x, y):
+		buffer = glSelectBuffer(len(self.sceneObjects)*4)
 		
+		glRenderMode(GL_SELECT)
+		
+		glMatrixMode(GL_PROJECTION)
+		glPushMatrix()
+		glLoadIdentity()
+		
+		projection = glGetDoublev(GL_PROJECTION_MATRIX)
+		viewport = glGetIntegerv(GL_VIEWPORT)
+		
+		print "X =", x
+		print "Y =", viewport[3] - y - 1
+		
+		gluPickMatrix(x, viewport[3] - y - 1, 1500, 1500, viewport)
+		glMultMatrixf(projection)
+		
+		glInitNames()
+		glPushName(0)
+		
+		for i in range(len(self.sceneObjects)):
+			print "LoadName =", i+1
+			print "SceneObject =", self.sceneObjects[i]
+			glLoadName(i)
+			self.sceneObjects[i].render(False)
+			
+		glMatrixMode(GL_PROJECTION)
+		glPopMatrix()
+		glFlush()
+			
+		hits = glRenderMode(GL_RENDER)
+		print "Hits = (%d) %s " % (len(hits), hits)
+		object = [0, 0]
+		for hit in hits:
+			near, far, names = hit
+			print "Near =", near
+			print "Far =", far
+			print "Names =", names
+			if (object[0] < far):
+				object = [far, names[0]]
+		print "Object =", object
+		
+		glPopName()
+		
+		for obj in self.sceneObjects:
+			if ((self.sceneObjects.index(obj)+1)==object[1]):
+				print "Selected!"
+				obj.selected = True
+				obj.changeColor()
+			else:
+				print "Not Selected!"
+				
+		self.updateGL()
--- src/objects.py
+++ src/objects.py
@@ -2,58 +2,79 @@ from OpenGL.GL import *
 from OpenGL.raw.GLUT import *
 
 class BaseObject(object):
-        """Base class for any object used in the GLWidget class."""
-        def __init__(self):
-                """Constructor of the base class. Usually, it should be called in the constructor of the inherited classes."""
-                self.rotationMatrix = []
-                self.centralPos = []
-                
-        def render(self):
-                """Virtual method that must be overridden in base-classes."""
-                pass
+    """Base class for any object used in the GLWidget class."""
+    
+    def __init__(self):
+        """Constructor of the base class. Usually, it should be called in the constructor of the inherited classes."""
         
-        def getRotationMatrix(self):
-                return self.rotationMatrix
-                        
-        def setRotationMatrix(self, rotationMatrix):
-                self.rotationMatrix = rotationMatrix
-                
-        def getCentralPosition(self):
-                return self.centralPos
-                        
-        def setCentralPosition(self, centralPosition):
-                self.centralPos = centralPosition
+        glColor3f(1, 0, 0)
+        self.rotationMatrix = []
+        self.centralPos = []
+        self.selected = False
+        
+        self.r = 1.0
+        self.g = 0.0
+        self.b = 0.0
+            
+    def render(self):
+        """Virtual method that must be overridden in base-classes."""
+        pass
+    
+    def changeColor(self):
+        if [self.r, self.g] == [1.0, 0.0]:
+            self.r = 0.0
+            self.g = 1.0
+        else:
+            self.r = 1.0
+            self.g = 0.0
+        self.render(True)
+    
+    def getRotationMatrix(self):
+        return self.rotationMatrix
+                    
+    def setRotationMatrix(self, rotationMatrix):
+        self.rotationMatrix = rotationMatrix
+            
+    def getCentralPosition(self):
+        return self.centralPos
+                    
+    def setCentralPosition(self, centralPosition):
+        self.centralPos = centralPosition
 
 class Cube(BaseObject):
-        """Class that defines a cube."""
+    """Class that defines a cube."""
+    
+    def __init__(self, side, wire=True):
+        """Constructor."""
         
-        def __init__(self, side, wire=True):
-                """Constructor."""
-                
-                super(Cube, self).__init__()
-                self.side = side
-                self.wire = wire
-                        
-        def render(self):
-                glTranslate(self.centralPos[0], self.centralPos[1], self.centralPos[2])
-                if self.wire:
-                        glutWireCube(self.side)
-                else:
-                        glutSolidCube(self.side)
+        super(Cube, self).__init__()
+        self.side = side
+        self.wire = wire
+                    
+    def render(self, translation=True):
+        if translation:
+            glColor3f(self.r, self.g, self.b)
+            glTranslate(self.centralPos[0], self.centralPos[1], self.centralPos[2])
+        if self.wire:
+                glutWireCube(self.side)
+        else:
+                glutSolidCube(self.side)
                         
 class Sphere(BaseObject):
-        """Class that defines a sphere."""
-        
-        def __init__(self, radius, wire=True):
-                """Constructor."""
-                super(Sphere, self).__init__()
-                self.radius = radius
-                self.wire = wire
-                
-        def render(self):
-                glTranslate(self.centralPos[0], self.centralPos[1], self.centralPos[2])
-                if self.wire:
-                        glutWireSphere(self.radius, 20, 20)
-                else:
-                        glutSolidSphere(self.radius, 20, 20)
+    """Class that defines a sphere."""
+    
+    def __init__(self, radius, wire=True):
+        """Constructor."""
+        super(Sphere, self).__init__()
+        self.radius = radius
+        self.wire = wire
+            
+    def render(self, translation=True):
+        if translation:
+            glColor3f(self.r, self.g, self.b)
+            glTranslate(self.centralPos[0], self.centralPos[1], self.centralPos[2])
+        if self.wire:
+                glutWireSphere(self.radius, 20, 20)
+        else:
+                glutSolidSphere(self.radius, 20, 20)
                         
