0f41f2abd0a33664be300b7e73229055444d6149
shinjikik <shinjikik@f16163c0-9e0f-11de-a8ff-c955b97a4a10>
2009-10-07 22:15:57 +0000

    - ArcBall ajeitado, quase nos trinques. - Rotação de objetos mostram uma wire sphere representando a ArcBall. - A posição da cena agora é sempre definida pela câmera (upVector, pointer, position). [TO-DO: Encapuslar câmera em objeto.] - Tratamento de clique com botão esquerdo corrigido. - Outros pequenos bugs corrigidos. - Cor do fundo mudada pra azul claro. - Vários pequenos refactors. - Render dos objetos não tem mais argumento "translate". - Diversas funções utilitárias adicionadas (util.py). - Atributos de posição/vetores agora seguem o estilo OpenGL: quarta componente indica se é vetor (0) ou ponto (1).

--- src/glwidget.py
+++ src/glwidget.py
@@ -11,216 +11,306 @@ from arcball import *
 from util import *
 
 class GlWidget(QGLWidget):
-	"""Our implementation of QGLWidget widget. :) - Joaquim, el gato."""
+	"""
+	Our implementation of QGLWidget widget. :) - Joaquim, el gato. Meow...
+	"""
 	
 	def __init__ (self, parent=None):
-		"""Widget constructor."""
+		"""
+		Widget constructor.
+		"""
 		
 		super(GlWidget, self).__init__(parent)
 		
-		# Enables mouse move events even if a button is not pressed.
+		# Enables mouse move events even if no button is pressed.
 		self.setMouseTracking(True)
+		
+		# Initialize window-related attributes.
+		self.wWidth, self.wHeight = 0, 0
 		
 		# Initialize widget attributes.
 		self.sceneObjects = []
-		self.rotation = numpy.identity(4) 
-		self.boundingSphere = ArcBall()
-		self.boundingSphere.setCentralPosition([0, 0, 0])
-		self.boundingSphere.setRadius(1)
+		self.selectedObjects = []
+		self.rotation = numpy.identity(4)
+		self.rotating = False
+		self.mousePos = numpy.zeros(3)
+		
+		# Camera's absolute position in world coordinates.
+		self.position = numpy.array([0.0, 0.0, 3.0, 1])
+		# Camera's up vector. Should always be unitary.
+		self.upVector = numpy.array([0.0, 1.0, 0.0, 0])
+		# Vector that points to the direction that the camera is looking. Always unitary.
+		self.pointer = numpy.array([0.0, 0.0, -1.0, 0])
+		# Camera's scale factor.
+		self.scale = numpy.array([1.0, 1.0, 1.0])
+		# Near/far clipping plane values.
+		self.near, self.far = 1, 101
+		# Perspective angle (in degrees).
+		self.fovAngle = 45
+		
+		# Scene's arcball.
+		self.boundingSphere = SceneArcBall(self)
 		
 		self.ctrl = False
 		self.isClicked = False
-		self.mousePosX = 0.0
-		self.mousePosY = 0.0
-		self.mousePosZ = 0.0
-		self.move = 0.0
 		self.z = 0.0
 		self.z2 = 0.0
-		self.zoom = 0.5
 		self.applyZoom = 0
 		
 	def initializeGL(self):
+		"""
+		Method called right before the first call to paintGL() or resizeGL().
+		"""
 		
 		glEnable(GL_DEPTH_TEST)
 		glEnable(GL_LIGHTING)
 		glEnable(GL_LIGHT0)
 		glEnable(GL_COLOR_MATERIAL)
+		glClearColor(0.6, 0.7, 0.9, 1)
 		
 	def paintGL(self):
-		"""Widget drawing callback."""
+		"""
+		Widget drawing callback.
+		"""
 		
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
 		self.render()
 
 	def resizeGL (self, width, height):
-		"""Resize callback."""
+		"""
+		Resize callback.
+		"""
 		
-		side = min(width,height)
-		glViewport((width - side) / 2, (height - side) / 2, side, side)
+		self.wWidth, self.wHeight = width, height
+		glViewport(0, 0, width, height)
 		self.updateGL()
 
 	def mouseMoveEvent(self, ev):
-		""""Mouse movement callback."""
-		self.z = self.mousePosY
+		"""
+		Mouse movement callback.
+		"""
+		
+		self.z = self.mousePos[Y]
 		if self.applyZoom:
 			if self.z > self.z2:
-				#self.zoom -= abs(self.zoom - self.z)/750
-				self.zoom += 0.2
-				self.z2 = self.mousePosY
+				self.position[Z] += 0.2
+				self.z2 = self.mousePos[Y]
 				self.updateMousePosition()
 				self.updateGL()
 			else:
-				#self.zoom += abs(self.zoom - self.z)/750
-				self.zoom -= 0.2
-				self.z2 = self.mousePosY
+				self.position[Z] -= 0.2
+				self.z2 = self.mousePos[Y]
 				self.updateMousePosition()
 				self.updateGL() 
 		
-		if (self.isClicked):
+		if self.isClicked:
 			self.updateMousePosition()
-			r = self.boundingSphere.setFinalPt(self.mousePosX, self.mousePosY)
-			self.rotation = self.rotation*r
-			self.update()
+			
+			if len(self.selectedObjects) == 0:
+				r = self.boundingSphere.setFinalPt(self.mousePos[X], self.mousePos[Y], True)
+				rotCenter = (self.position + self.pointer*10)[:3]
+				
+				glMatrixMode(GL_MODELVIEW)
+				glPushMatrix()
+				glLoadIdentity()
+				glTranslate(*rotCenter)
+				glMultMatrixd(r)
+				glTranslate(*(-rotCenter))
+				self.position = multiplyByMatrix(self.position)
+				self.upVector = multiplyByMatrix(self.upVector)
+				self.pointer = multiplyByMatrix(self.pointer)
+				glPopMatrix()
+			else:
+				for obj in self.selectedObjects:
+					obj.rightClickMoveEvent(self.mousePos[X], self.mousePos[Y])
+			
+			self.updateGL()
 		
 	def mousePressEvent(self, ev):
-		"""Mouse press callback."""
+		"""
+		Mouse press callback.
+		"""
 		
 		self.updateMousePosition()
 		btn = ev.button()
 		if (btn == Qt.LeftButton):
-			btn = "Left"
-			self.pickingObjects(self.mousePosX, self.mousePosY)
+			self.handlePicking(self.mousePos[X], self.mousePos[Y])
 		elif (btn == Qt.RightButton):
-			btn = "Right"
 			self.isClicked = True
-			self.boundingSphere.setInitialPt(self.mousePosX, self.mousePosY)
-			#self.applyZoom = True
-			#self.z = self.mousePosY
+			if len(self.selectedObjects) == 0:
+				self.boundingSphere.setInitialPt(self.mousePos[X], self.mousePos[Y])
+				self.rotating = True
+			else:
+				for obj in self.selectedObjects:
+					obj.rightClickEvent(self.mousePos[X], self.mousePos[Y])
 		elif (btn == Qt.MidButton):
-			btn = "Middle"
 			self.applyZoom = True
-			self.z = self.mousePosY
-		else:
-			btn = "Joaquim"
-		print "Click: %s (%i, %i)" % (btn, self.mousePosX, self.mousePosY)
-#		
-	def mouseReleaseEvent(self, ev):
-		"""Mouse release callback."""
+			self.z = self.mousePos[Y]
+			
+		print "Click (%i, %i)" % (self.mousePos[X], self.mousePos[Y])
 		
-		self.isClicked = False
-		self.applyZoom = False	 
+	def mouseReleaseEvent(self, ev):
+		"""
+		Mouse release callback.
+		"""
+		
+		self.updateMousePosition()
+		
+		if self.isClicked:
+			if len(self.selectedObjects) > 0:
+				for obj in self.selectedObjects:
+					obj.rightClickReleaseEvent(self.mousePos[X], self.mousePos[Y])
+			else:
+				self.rotating = False
+			self.isClicked = False
+			self.updateGL()
+			
+		self.applyZoom = False
 
 	def enterEvent(self, ev):
-		"""Event called when the mouse enters the widget area."""
+		"""
+		Event called when the mouse enters the widget area.
+		"""
 		
 		self.setFocus()
 		self.grabKeyboard()
 		
 	def leaveEvent(self, ev):
-		"""Event called when the mouse leaves the widget area."""
+		"""
+		Event called when the mouse leaves the widget area.
+		"""
 		
 		self.parent().setFocus()
 		self.releaseKeyboard()
 
 	def keyPressEvent(self, ev):
-		"""Key press callback."""
+		"""
+		Key press callback.
+		"""
 		
 		self.updateMousePosition()
 		key = str(ev.text()).upper()
-		if (ev.modifiers() == Qt.ControlModifier):
+		if (ev.modifiers() & Qt.ControlModifier):
 			self.ctrl = True
 		if (key == "C"):
-			newCube = Cube(0.5, False)
+			newCube = Cube(0.5, self, False)
 			newCube.setCentralPosition(self.getMouseScenePosition())
 			newCube.setRotationMatrix(self.rotation)
 			self.sceneObjects.append(newCube)
 			self.updateGL()
 		elif (key == "E"):
-			newSphere = Sphere(0.5, False)
+			newSphere = Sphere(0.5, self, False)
 			newSphere.setCentralPosition(self.getMouseScenePosition())
 			newSphere.setRotationMatrix(self.rotation)
 			self.sceneObjects.append(newSphere)
 			self.updateGL()
+		elif (key == "X"):
+			for obj in self.selectedObjects:
+				self.sceneObjects.remove(obj)
+			del self.selectedObjects[:]
+			self.updateGL()
 		elif (key == "W"):
-			self.zoom += 0.2
+			self.position[Y] += 0.2
 			self.updateGL()
 		elif (key == "S"):
-			self.zoom -= 0.2
+			self.position[Y] -= 0.2
 			self.updateGL()
 		elif (key == "A"):
-			self.move += 0.3
+			self.position[X] += 0.3
 			self.updateGL()
 		elif (key == "D"):
-			self.move -= 0.3
+			self.position[X] -= 0.3
 			self.updateGL()
+		elif (ev.key() == Qt.Key_Home):
+			pass
 		else:
 			pass
 		
 	def keyReleaseEvent(self, ev):
-		self.ctrl = False
+		if (ev.key() == Qt.Key_Control):
+			self.ctrl = False
 		
 	def updateMousePosition(self):
-		"""Updates mousePosX and mousePosY attributes, using GL coordinates."""
+		"""
+		Updates mousePos[X] and mousePos[Y] attributes, using GL coordinates.
+		"""
 		
 		screenY = self.mapFromGlobal(QCursor.pos()).y()
-		self.mousePosX = self.mapFromGlobal(QCursor.pos()).x()
-		self.mousePosY = glGetIntegerv(GL_VIEWPORT)[3] - screenY - 1
+		self.mousePos[X] = self.mapFromGlobal(QCursor.pos()).x()
+		self.mousePos[Y] = glGetIntegerv(GL_VIEWPORT)[3] - screenY - 1
 			
 	def getMouseScenePosition(self):
-		"""Gets the coordinates of the mouse in the scene. Since we cannot recover the Z coordinate, so we just give it a fixed value: 0.5."""
+		"""
+		Gets the coordinates of the mouse in the scene.
+		Since we cannot recover the Z coordinate, we just give it a fixed value: 0.5.
+		"""
 		
 		self.updateMousePosition()
-		#self.mousePosZ = glReadPixels(self.mousePosX, self.mousePosY, 1, 1, 
+		#self.mousePosZ = glReadPixels(self.mousePos[X], self.mousePos[Y], 1, 1, 
 		#							  GL_DEPTH_COMPONENT, GL_FLOAT)
-		self.mousePosZ = 0.5
+		self.mousePos[Z] = 0.5
 		
-		self.coordPosition = gluUnProject(self.mousePosX, self.mousePosY, self.mousePosZ,
-										  glGetDoublev(GL_MODELVIEW_MATRIX),
-										  glGetDoublev(GL_PROJECTION_MATRIX),
-										  glGetIntegerv(GL_VIEWPORT))
+		coordPosition = gluUnProject(*self.mousePos)
 		
-		return self.coordPosition
-		
-	def render(self):
-				
-		glMatrixMode(GL_PROJECTION)
-		glLoadIdentity()
-		gluPerspective(45, 1, 1, 500)
-		
-		glTranslate(self.move,0,self.zoom)
+		return arrayToVector(coordPosition, 1)
+	
+	def __setCameraView(self):
+		"""
+		Sets the camera to the current lookAt position and rotation angle.
+		Warning: This method sets the matrix mode to GL_MODELVIEW.
+		"""
 		
 		glMatrixMode(GL_MODELVIEW)
 		glLoadIdentity()
-		glTranslate(0, 0, -3)
-		glMultMatrixf(self.rotation)
-		#gluLookAt(self.move, 0.0, self.zoom, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0)
 		
-		light0_pos = [20.0, 20.0, 20.0, 100.0]
+		gluLookAt(self.position[X], self.position[Y], self.position[Z], \
+				self.position[X] + self.pointer[X], self.position[Y] + self.pointer[Y], self.position[Z] + self.pointer[Z], \
+				self.upVector[X], self.upVector[Y], self.upVector[Z])
+		glScale(self.scale[X], self.scale[Y], self.scale[Z])
+		
+		# Create a small white sphere in the center of the scene's rotation center, just for refernece.
+		if self.rotating:
+			rotCenter = (self.position + self.pointer*10)[:3]
+			glColor(1, 1, 1)
+			glTranslate(*rotCenter)
+			glutSolidSphere(0.1, 20, 20)
+			glTranslate(*-rotCenter)
+		
+	def render(self):
+		"""
+		Renders the scene.
+		Warning: This method sets the matrix mode to GL_MODELVIEW.
+		"""
+		
+		glMatrixMode(GL_PROJECTION)
+		glLoadIdentity()
+		gluPerspective(self.fovAngle, float(self.wWidth)/self.wHeight, self.near, self.far)
+		
+		self.__setCameraView()
+		
+		light0_pos = [1.0, 1.0, 1.0, 0]
 		diffuse0 = [1.0, 1.0, 1.0, 1.0]
 		specular0 = [0, 0, 0, 1.0]
-		ambient0 = [0, 0, 0, 1.0]
+		ambient0 = [0.2, 0.2, 0.2, 1.0]
 
 		glLightfv(GL_LIGHT0, GL_POSITION, light0_pos)
 		glLightfv(GL_LIGHT0, GL_DIFFUSE, diffuse0)
 		glLightfv(GL_LIGHT0, GL_SPECULAR, specular0)
 		glLightfv(GL_LIGHT0, GL_AMBIENT, ambient0)
 		
-		
-		
-		
 		# Create a small white wire sphere in the 0 coordinate, just for refernece.
 		glColor(1, 1, 1)
 		glutWireSphere(0.1, 20, 20)
-		
-		glColor(1, 1, 1)
-		glutWireSphere(1, 20, 20)
 		
 		for obj in self.sceneObjects:
 			glPushMatrix()
 			obj.render()
 			glPopMatrix()
 			
-	def pickingObjects(self, x, y):
+	def handlePicking(self, x, y):
+		"""
+		Handles object picking when the user clicks with the mouse's left button.
+		"""
 		
 		buffer = glSelectBuffer(len(self.sceneObjects)*4)
 		projection = glGetDoublev(GL_PROJECTION_MATRIX)
@@ -231,20 +321,18 @@ class GlWidget(QGLWidget):
 		glMatrixMode(GL_PROJECTION)
 		glPushMatrix()
 		glLoadIdentity()
-		gluPickMatrix(x, y, 15, 15, viewport)
+		gluPickMatrix(x, y, 2, 2, viewport)
 		glMultMatrixf(projection)
 		
-		glMatrixMode(GL_MODELVIEW)
-		glLoadIdentity()
-		glTranslate(0, 0, -3)
-		glMultMatrixf(self.rotation)
+		self.__setCameraView()
+		
 		glInitNames()
 		glPushName(0)
 		
 		for i in range(len(self.sceneObjects)):
 			glPushMatrix()
 			glLoadName(i)
-			self.sceneObjects[i].render(True)
+			self.sceneObjects[i].render()
 			glPopMatrix()
 				
 		glMatrixMode(GL_PROJECTION)
@@ -255,26 +343,45 @@ class GlWidget(QGLWidget):
 		glPopName()
 			
 		hits = glRenderMode(GL_RENDER)
-		#print "Hits = (%d) %s " % (len(hits), hits)
 		
 		nearestHit = None
 		for hit in hits:
 			near, far, names = hit
-			print near, far
 			if (nearestHit == None) or (near < nearestHit[0]):
 				nearestHit = [near, names[0]]
-				
-		pickedObject = self.sceneObjects[nearestHit[1]]
 		
 		if nearestHit != None:
-			unselectedObjects = 0
-			if not self.ctrl:
-				for obj in self.sceneObjects:
-					if (obj != pickedObject) and (obj.selectionStatus()):
-						obj.selectObject()
-						unselectedObjects += 1
+			# An object was picked.
+			pickedObject = self.sceneObjects[nearestHit[1]]
 			
-			if (unselectedObjects == 0) or (not(pickedObject.selectionStatus())):
-				pickedObject.selectObject()
-				
+			if not(self.ctrl):
+				# CTRL is not pressed.
+				if len(self.selectedObjects) == 1 \
+				and self.selectedObjects[0] == pickedObject:
+					# The only object that was previously selected is the one picked.
+					pickedObject.select(False)
+					del self.selectedObjects[:]
+				else:
+					# Deselect all previously selected objects.
+					for obj in self.selectedObjects:
+						obj.select(False)
+					del self.selectedObjects[:]
+					
+					# Select the picked object.
+					pickedObject.select(True)
+					self.selectedObjects.append(pickedObject)
+			else:
+				# CTRL is pressed.
+				if pickedObject.isSelected():
+					pickedObject.select(False)
+					self.selectedObjects.remove(pickedObject)
+				else:
+					pickedObject.select(True)
+					self.selectedObjects.append(pickedObject)
+		elif not(self.ctrl):
+			# No objects were picked and CTRL is not pressed.
+			for obj in self.selectedObjects:
+				obj.select(False)
+			del self.selectedObjects[:]
+			
 		self.updateGL()
--- src/objects.py
+++ src/objects.py
@@ -2,88 +2,119 @@ from OpenGL.GL import *
 from OpenGL.raw.GLUT import *
 from arcball import *
 from util import *
+from math import sqrt
 
 class BaseObject(object):
 	"""Base class for any object used in the GLWidget class."""
 	
-	def __init__(self):
+	def __init__(self, parent):
 		"""Constructor of the base class. Usually, it should be called in the constructor of the inherited classes."""
 		
-		self.rotationMatrix = []
+		self.rotation = []
 		self.centralPos = []
-		self.selected = False
 		
-		self.r = 1.0
-		self.g = 0.0
-		self.b = 0.0
+		# Radius of the bouding sphere that surrounds the object. 
+		self.radius = 0.0
+		self.selected = False
+		self.rotating = False
+		
+		# Initial color of the object.
+		self.r, self.g, self.b = 1.0, 0.0, 0.0
+		
+		# Reference to the GLWidget object that contains this object.
+		self.parent = parent
+		self.arcBall = ArcBall(self.parent)
 			
 	def render(self):
 		"""Virtual method that should be overridden in base-classes."""
 		pass
-	
-	def changeColor(self):
-		if [self.r, self.g] == [1.0, 0.0]:
-			self.r = 0.0
-			self.g = 1.0
-		else:
-			self.r = 1.0
-			self.g = 0.0
 			
-	def selectionStatus(self):
+	def isSelected(self):
 		return self.selected
 			
-	def selectObject(self):
-		self.selected = not(self.selected)
-		self.changeColor()
+	def select(self, newStatus):
+		"""Selects or unselects the object, depending on the newStatus argument.
+		Also changes the object's color according to the selection status."""
+		
+		self.selected = newStatus
+		if self.selected:
+			self.r, self.g, self.b = 0, 1, 0
+		else:
+			self.r, self.g, self.b = 1, 0, 0
+			
+	def rightClickEvent(self, x, y):
+		self.rotating = True
+		self.arcBall.setInitialPt(x, y)
+	
+	def rightClickMoveEvent(self, x, y):
+		r = self.arcBall.setFinalPt(x, y)
+		self.rotation = matrixByMatrix(r, self.rotation)
+		
+	def rightClickReleaseEvent(self, x, y):
+		self.rotating = False
 	
 	def getRotationMatrix(self):
-		return self.rotationMatrix
+		return self.rotation
 		
 	def setRotationMatrix(self, rotationMatrix):
-		self.rotationMatrix = rotationMatrix
-			
+		self.rotation = rotationMatrix
+		
 	def getCentralPosition(self):
 		return self.centralPos
 		
 	def setCentralPosition(self, centralPosition):
 		self.centralPos = centralPosition
+		self.arcBall.setCentralPosition(self.centralPos)
+		
+	def setRadius(self, newRadius):
+		self.radius = newRadius
+		self.arcBall.radius = self.radius
 
 class Cube(BaseObject):
 	"""Class that defines a cube."""
 	
-	def __init__(self, side, wire=True):
+	def __init__(self, side, parent, wire=True):
 		"""Constructor."""
 		
-		super(Cube, self).__init__()
+		super(Cube, self).__init__(parent)
 		self.side = side
 		self.wire = wire
+		self.setRadius(side * sqrt(3) / 2.0)
 		
-	def render(self, translation=True):
-		if translation:
-			glColor3f(self.r, self.g, self.b)
-			glTranslate(self.centralPos[X], self.centralPos[Y], self.centralPos[Z])
-			glMultMatrixf(self.rotationMatrix)
+	def render(self):
+		glColor3f(self.r, self.g, self.b)
+		glTranslate(self.centralPos[X], self.centralPos[Y], self.centralPos[Z])
+		glMultMatrixf(self.rotation)
 		if self.wire:
 			glutWireCube(self.side)
 		else:
 			glutSolidCube(self.side)
+			
+		if self.rotating:
+			glColor(0.3, 0.6, 1)
+			glutWireSphere(self.radius + 0.005, 20, 20)
 		
 class Sphere(BaseObject):
 	"""Class that defines a sphere."""
 	
-	def __init__(self, radius, wire=True):
+	def __init__(self, radius, parent, wire=True):
 		"""Constructor."""
-		super(Sphere, self).__init__()
-		self.radius = radius
-		self.wire = wire
 		
-	def render(self, translation=True):
-		if translation:
-			glColor3f(self.r, self.g, self.b)
-			glTranslate(self.centralPos[X], self.centralPos[Y], self.centralPos[Z])
-			glMultMatrixf(self.rotationMatrix)
+		super(Sphere, self).__init__(parent)
+		self.wire = wire
+		self.setRadius(radius)
+		
+	def render(self):
+		glColor(self.r, self.g, self.b)
+		glTranslate(self.centralPos[X], self.centralPos[Y], self.centralPos[Z])
+		glMultMatrixf(self.rotation)
+		
 		if self.wire:
 			glutWireSphere(self.radius, 20, 20)
 		else:
 			glutSolidSphere(self.radius, 20, 20)
+			
+		if self.rotating:
+			glColor(0.3, 0.6, 1)
+			glutWireSphere(self.radius + 0.005, 20, 20)
 		
